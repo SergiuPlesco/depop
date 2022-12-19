edf89df34803f0a202ad380853f3c60c6df80999
Sergiu Plesco <sergiu.plesco@smartdata.solutions>
2022-12-19 16:11:47 +0200

    added submenu list, styled submenu list items

--- client/src/components/AppNavigation/MobileNavigation/MainNavigationItem.tsx
+++ client/src/components/AppNavigation/MobileNavigation/MainNavigationItem.tsx
@@ -3,37 +3,53 @@ import ArrowRight from "../../../assets/ArrowRight";
 import Submenu from "./Submenu";
 
 type MainNavigationItemType = {
-	menu: {
-		name: string;
-	};
+  closeMobileMenu: () => void;
+  menu: {
+    name: string;
+    submenu: {
+      [key: string]: {
+        name: string;
+        link: string;
+      };
+    };
+  };
 };
 
-const MainNavigationItem: React.FC<MainNavigationItemType> = ({ menu }) => {
-	const [isSubmenuVisible, setIsSubmenuVisible] = useState(false);
-	const openSubmenu = () => {
-		setIsSubmenuVisible(true);
-	};
-	const closeSubmenu = () => {
-		setIsSubmenuVisible(false);
-	};
+const MainNavigationItem: React.FC<MainNavigationItemType> = ({
+  menu,
+  closeMobileMenu,
+}) => {
+  const [isSubmenuVisible, setIsSubmenuVisible] = useState(false);
+  const openSubmenu = () => {
+    setIsSubmenuVisible(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuVisible(false);
+  };
 
-	return (
-		<li key={menu.name} className="border-b">
-			<button
-				className="flex justify-between items-center cursor-pointer py-3 w-full font-light tracking-wide text-gray-600 text-sm"
-				onClick={() => {
-					openSubmenu();
-				}}
-			>
-				<span className="block">{menu.name}</span>
-				<span className="block">
-					<ArrowRight />
-				</span>
-			</button>
+  return (
+    <li className="border-b">
+      <button
+        className="flex justify-between items-center cursor-pointer py-3 w-full font-light tracking-wide text-gray-600 text-sm"
+        onClick={() => {
+          openSubmenu();
+        }}
+      >
+        <span className="block">{menu.name}</span>
+        <span className="block">
+          <ArrowRight />
+        </span>
+      </button>
 
-			<Submenu title={menu.name} closeSubmenu={closeSubmenu} isSubmenuVisible={isSubmenuVisible} />
-		</li>
-	);
+      <Submenu
+        title={menu.name}
+        closeSubmenu={closeSubmenu}
+        isSubmenuVisible={isSubmenuVisible}
+        submenu={menu.submenu}
+        closeMobileMenu={closeMobileMenu}
+      />
+    </li>
+  );
 };
 
 export default MainNavigationItem;
--- client/src/components/AppNavigation/MobileNavigation/Submenu.tsx
+++ client/src/components/AppNavigation/MobileNavigation/Submenu.tsx
@@ -4,49 +4,91 @@ import Xmark from "../../../assets/Xmark";
 import styled, { css } from "styled-components";
 
 type SubmenuProps = {
-	title: string;
-	closeSubmenu: () => void;
-	isSubmenuVisible: boolean;
+  title: string;
+  closeSubmenu: () => void;
+  closeMobileMenu: () => void;
+  isSubmenuVisible: boolean;
+  submenu: {
+    [key: string]: {
+      name: string;
+      link: string;
+    };
+  };
 };
 
 type SubmenuContainerProps = {
-	isSubmenuVisible: boolean;
+  isSubmenuVisible: boolean;
 };
 
-const Submenu: React.FC<SubmenuProps> = ({ title, closeSubmenu, isSubmenuVisible }) => {
-	return (
-		<SubmenuContainer isSubmenuVisible={isSubmenuVisible}>
-			<div className="flex justify-between items-center">
-				<button className="block" onClick={closeSubmenu}>
-					<ArrowLeft />
-				</button>
-				<button className="block">
-					<Xmark />
-				</button>
-			</div>
-			<div className="relative w-full">{title}</div>
-		</SubmenuContainer>
-	);
+const Submenu: React.FC<SubmenuProps> = ({
+  title,
+  closeSubmenu,
+  closeMobileMenu,
+  isSubmenuVisible,
+  submenu,
+}) => {
+  return (
+    <SubmenuContainer isSubmenuVisible={isSubmenuVisible}>
+      <div className="w-full relative">
+        <div className="flex justify-between items-center pb-6">
+          <button className="block" onClick={closeSubmenu}>
+            <ArrowLeft />
+          </button>
+          <button className="block" onClick={closeMobileMenu}>
+            <Xmark />
+          </button>
+        </div>
+        <div className="relative w-full">
+          <a
+            href="#"
+            rel="noopener noreferrer"
+            className="block text-gray-800 no-underline font-bold text-lg"
+          >
+            {title}
+          </a>
+        </div>
+        <div className="mt-8 mb-2">
+          <ul>
+            {/* used for bottom border only */}
+            <li className="border-b"></li>
+            {Object.values(submenu).map((submenu) => {
+              return (
+                <li key={submenu.name} className="border-b">
+                  <a
+                    href="#"
+                    className="flex justify-between items-center py-3 cursor-pointer no-underline w-full text-sm m-0 text-gray-600"
+                  >
+                    {submenu.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </SubmenuContainer>
+  );
 };
 
 export default Submenu;
 
 const SubmenuContainer = styled.div<SubmenuContainerProps>`
-	position: fixed;
-	top: 0;
-	bottom: 0;
-	width: 100%;
-	height: 100%;
-	z-index: 100;
-	background-color: #fff;
-	padding: 1.5rem;
-	overflow-y: auto;
-	${(props) =>
-		props.isSubmenuVisible
-			? css`
-					left: 0;
-			  `
-			: css`
-					left: -100%;
-			  `}
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+  background-color: #fff;
+  padding: 1.5rem;
+  overflow-y: auto;
+  transition: all 0.4s ease 0s;
+  ${(props) =>
+    props.isSubmenuVisible
+      ? css`
+          left: 0;
+        `
+      : css`
+          left: -100%;
+        `}
 `;
